d382bbb6f943cc73b0874ca3852e3e9fd43d315c
Allan Tang (saasbook) <allan_tang@berkeley.edu>
2015-03-15 16:21:59 -0700

    Passing 80/100 on previous commit. Changing comment logic for merging articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -416,12 +416,12 @@ class Article < Content
     user.admin? || user_id == user.id
   end
 
-  def merge_with(merge_article, merge_id)
+  def merge_with(merge_article)
     self.body += merge_article.body || ""
     self.extended += merge_article.extended || ""
     self.excerpt += merge_article.excerpt || ""
-    Comment.find_all_by_id(merge_id).each do |comment|
-      comment.article_id = self.id
+    merge_article.comments.each do |comment|
+      self.comments << comment
     end
     self.save
     merge_article.destroy
